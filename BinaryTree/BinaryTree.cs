c77213dd3192522bf3959aa1585e57ce5126f8d5
Akhil <akhilu5042@gmail.com>
2022-01-22 23:09:56 +0530

    BinaryTree

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree<T> where T : IComparable<T>
+    {/* UC1:- Ability to create a BST by adding 56 and then adding 30 & 70 
+              - Use INode to create My Binary Node - Note the key has to extend comparable to compare
+              and determine left or right node
+              - First add 56 as root node so 30 will be added to left and 70 to right.
+      */
+        public T NodeData { get; set; }
+        public BinaryTree<T> LeftTree { get; set; }
+        public BinaryTree<T> RightTree { get; set; }
+        public BinaryTree(T nodeData) //Constructor
+        {
+            NodeData = nodeData;
+            LeftTree = null;
+            RightTree = null;
+        }
+        int leftCount = 0; //initilize
+        int rightCount = 0;
+        bool result = false;
+        public void Insert(T item) //insert Method
+        {
+            try
+            {
+                T CurrentNodeValue = this.NodeData;
+                if (CurrentNodeValue.CompareTo(item) > 0) //check left Tree value is > 0
+                {
+                    if (this.LeftTree == null) //check lefttree null
+                    {
+                        this.LeftTree = new BinaryTree<T>(item); //add
+                    }
+                    else
+                    {
+                        this.LeftTree.Insert(item);
+                    }
+                }
+                else
+                {
+                    if (this.RightTree == null) //Check Right Tree value is > 0
+                    {
+                        this.RightTree = new BinaryTree<T>(item);
+                    }
+                    else
+                    {
+                        this.RightTree.Insert(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public void GetSize()
+        {
+            Console.WriteLine($"Size   {1 + this.leftCount + this.rightCount}"); //print tree size
+        }
+        public void Display() //Display Method
+        {
+            try
+            {
+                if (this.LeftTree != null)
+                {
+                    this.leftCount++;
+                    this.LeftTree.Display(); //Display Leftside value
+                }
+                Console.WriteLine(this.NodeData.ToString());
+                if (this.RightTree != null)
+                {
+                    this.rightCount++;
+                    this.RightTree.Display(); //Display RightSide value
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public bool Search(T element, BinaryTree<T> node) //create Method to Search Element in BST
+        {
+            try
+            {
+                if (node == null) //node is empty element not exist
+                {
+                    Console.WriteLine($"Element Not Found In Binary Search Tree { node.NodeData}"); //print
+                    return false;
+                }
+                if (node.NodeData.Equals(element)) //element match exist
+                {
+                    Console.WriteLine($"Element Found In Binary Search Tree {node.NodeData}"); //print
+
+                    result = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Current Element is {node.NodeData} in Binary Search Tree");
+
+                }
+
+                if (element.CompareTo(node.NodeData) < 0)
+                {
+                    Search(element, node.LeftTree);
+                }
+                if (element.CompareTo(node.NodeData) > 0)
+                {
+                    Search(element, node.RightTree);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return result;
+
+        }
+
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinaryTree;
+
+Console.WriteLine("Hello, World!");
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("\nEnter the program number which is to be executed \n 1.UC-1, \n 2.UC-2, \n 3.UC-3, \n 4.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("BinarySearchTree");
+            BinaryTree<int> bst = new BinaryTree<int>(56); //Create object and pass root value of tree
+            bst.Insert(30); //Add Element
+            bst.Insert(70);
+
+            bst.Display(); //call Display Method
+            Console.WriteLine("         56        ---> Root \n");
+            Console.WriteLine("     30     70");
+
+            Console.ReadLine();
+            break;
+        case 2:
+            Console.WriteLine("BinarySearchTree");
+            BinaryTree<int> bst1 = new BinaryTree<int>(56); //Create object and pass root value of tree
+            bst1.Insert(30); //Add Element
+            bst1.Insert(70);
+            bst1.GetSize(); //call GetSize method to print size //UC2
+
+
+            bst1.Display(); //call Display Method
+            Console.WriteLine("         56        ---> Root \n");
+            Console.WriteLine("     30     70");
+            Console.WriteLine("          ");
+
+            Console.ReadLine();
+            break;
+        case 3:
+            Console.WriteLine("BinarySearchTree");
+            BinaryTree<int> bst2 = new BinaryTree<int>(56); //Create object and pass root value of tree
+            bst2.Insert(30); //Add Element
+            bst2.Insert(70);
+            bst2.Insert(22);
+            bst2.Insert(40);
+            bst2.Insert(60);
+            bst2.Insert(95);
+            bst2.Insert(11);
+            bst2.Insert(65);
+            bst2.Insert(3);
+            bst2.Insert(16);
+            bst2.Insert(63);
+            bst2.Insert(67);
+            bst2.GetSize(); //call GetSize method to print size //UC2
+
+            bst2.Search(70, bst2);
+
+
+            bst2.Display(); //call Display Method
+            Console.WriteLine("                                           56                    ---> Root \n");
+            Console.WriteLine("                                 30                    70                    ");
+            Console.WriteLine("                          22           40         60             95            ");
+            Console.WriteLine("                     11                                65                  ");
+            Console.WriteLine("                3          16                   63           67           ");
+
+
+
+            Console.ReadLine();
+            break;
+        case 4:
+            flag = false;
+            break;
+    }
+}
